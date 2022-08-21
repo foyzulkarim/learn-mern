d63acccbaed87fa1eec29dc095b7da81cd7005ae
Foyzul Karim <foyzulkarim@gmail.com>
2022-08-21 22:20:01 +1000

    create collection wip

--- mongos/exapp1/mongo.js
+++ mongos/exapp1/mongo.js
@@ -1,6 +1,58 @@
 // import the mongoclient
 const { MongoClient } = require("mongodb");
 
+const createCollections = async (db) => {
+  const names = (await _db.collections()).map((n) => n.s.namespace.collection);
+  console.dir(names);
+  const students = "students";
+  if (!names.find((n) => n === students)) {
+    console.log("creating ", students);
+    db.createCollection(students, {
+      validator: {
+        $jsonSchema: {
+          bsonType: "object",
+          required: ["name", "year", "major", "address"],
+          properties: {
+            name: {
+              bsonType: "string",
+              description: "must be a string and is required",
+            },
+            year: {
+              bsonType: "int",
+              minimum: 2017,
+              maximum: 3017,
+              description:
+                "must be an integer in [ 2017, 3017 ] and is required",
+            },
+            major: {
+              enum: ["Math", "English", "Computer Science", "History", null],
+              description: "can only be one of the enum values and is required",
+            },
+            gpa: {
+              bsonType: ["double"],
+              description: "must be a double if the field exists",
+            },
+            address: {
+              bsonType: "object",
+              required: ["city"],
+              properties: {
+                street: {
+                  bsonType: "string",
+                  description: "must be a string if the field exists",
+                },
+                city: {
+                  bsonType: "string",
+                  description: "must be a string and is required",
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+};
+
 let _db = null;
 // create a connect
 const connect = async () => {
@@ -10,6 +62,7 @@ const connect = async () => {
   console.log("connecting to MongoDB");
   await client.connect();
   _db = client.db("schooldb");
+  createCollections(_db);
 };
 
 // create a getdb
