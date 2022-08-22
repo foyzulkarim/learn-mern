d8a4cb52a06923699ad1a070194cc6f388b9d94f
Foyzul Karim <foyzulkarim@gmail.com>
2022-08-22 15:02:11 +1000

    cleanup

--- mongos/exapp1/mongo.js
+++ mongos/exapp1/mongo.js
@@ -1,43 +1,6 @@
 // import the mongoclient
 const { MongoClient } = require("mongodb");
 
-const createCollections = async (db) => {
-  const names = (await _db.collections()).map((n) => n.s.namespace.collection);
-  console.dir(names);
-  const students = "students";
-  if (!names.find((n) => n === students)) {
-    console.log("creating ", students);
-    db.createCollection(students, {
-      validator: {
-        $jsonSchema: {
-          bsonType: "object",
-          required: ["name", "phone", "age", "city"],
-          properties: {
-            name: {
-              bsonType: "string",
-              description: "must be a string and is required",
-            },
-            phone: {
-              bsonType: ["string"],
-              description: "must be a string and is required",
-            },
-            age: {
-              bsonType: "int",
-              minimum: 0,
-              maximum: 200,
-              description: "must be an integer in [ 0, 200 ] and is required",
-            },
-            city: {
-              enum: ["Dhaka", "Chittagong", "Rajshahi", "Khulna", "Sylhet"],
-              description: "can only be one of the enum values and is required",
-            },
-          },
-        },
-      },
-    });
-  }
-};
-
 let _db = null;
 // create a connect
 const connect = async () => {
@@ -47,26 +10,15 @@ const connect = async () => {
   console.log("connecting to MongoDB");
   await client.connect();
   _db = client.db("schooldb");
-  //createCollections(_db);
 };
 
 // create a getdb
 const getDb = () => {
-  // if (!_db) {
-  //   await connect();
-  // }
   return _db;
-};
-
-const getCollections = () => {
-  return {
-    Student: _db?.collection("students"),
-  };
 };
 
 // export them
 module.exports = {
   connect,
   getDb,
-  getCollections,
 };
