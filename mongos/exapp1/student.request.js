ceb54cca462325494e4cd9c2633cbc4f99cb961d
Foyzul Karim <foyzul@mentorloop.com>
2022-11-21 17:49:52 +1100

    fixed number validation

--- mongos/exapp1/student.request.js
+++ mongos/exapp1/student.request.js
@@ -5,7 +5,7 @@ const schema = Joi.object().keys({
   _id: Joi.string().optional(),
   name: Joi.string().min(3).max(30).required(),
   phone: Joi.string().min(3).max(30).required(),
-  age: Joi.number().min(0).max(200).required(),
+  age: Joi.number().strict().min(0).max(200).required(),
   city: Joi.string().valid(...BANGLADESH_CITIES),
 });
 
